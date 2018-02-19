0ace1d1d3b49cc311c459edd05eb6dc46bba5e3c
ZipCPU <dgisselq@ieee.org>
2018-02-19 10:30:26 -0500

    Fleshed out the (work in progress) repo a touch

---
+++ sw/board/hello.c
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename:	helloworld.c
+//
+// Project:	TinyZip, a demonstration project for the TinyFPGA B2 board
+//
+// Purpose:	The original Helllo World program.  If everything works, this
+//		will print Hello World to the UART, and then halt the CPU--if
+//	run with no O/S.
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Gisselquist Technology asserts no ownership rights over this particular
+// hello world program.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Hello, World!\n");
+}
